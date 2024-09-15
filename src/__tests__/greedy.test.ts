1030902cfd745a30525d13d5c5bd1dc05e15a0d0
Pratik Thapa <pratik.philos@live.com>
2024-09-15 07:42:39 -0400

    assign cookies problem and tests

--- src/__tests__/greedy.test.ts
+++ src/__tests__/greedy.test.ts
@@ -1,4 +1,4 @@
-import { canPlaceFlowers } from "@algorithms/greedy";
+import { assignCookies, canPlaceFlowers } from "@algorithms/greedy";
 
 const testCases = [
   {
@@ -17,5 +17,24 @@ const testCases = [
 testCases.forEach(({ arr, expected, n }, index) => {
   test(`Greedy: FlowerBed Problem ${index + 1}`, () => {
     expect(canPlaceFlowers(arr, n)).toEqual(expected);
+  });
+});
+
+const cookiesTestCases = [
+  {
+    g: [1, 2, 3],
+    s: [1, 1],
+    expected: 1,
+  },
+  {
+    g: [1, 2],
+    s: [1, 2, 3],
+    expected: 2,
+  },
+];
+
+cookiesTestCases.forEach(({ g, s, expected }, index) => {
+  test(`Assign Cookies to Happy Children. Problem ${index}`, () => {
+    expect(assignCookies(g, s)).toEqual(expected);
   });
 });
--- src/algorithms/greedy.ts
+++ src/algorithms/greedy.ts
@@ -1,3 +1,6 @@
+/**
+ * @description: From https://leetcode.com/problems/can-place-flowers/
+ * */
 export function canPlaceFlowers(arr: number[], n: number): boolean {
   let count = 0;
   // check if adjacent positions are 1. Make sure you account for edge cases
@@ -16,6 +19,7 @@ export function canPlaceFlowers(arr: number[], n: number): boolean {
   // at most n should be present
   return count >= n;
 }
+// copied solution
 export function canPlaceFlowersCopied(flowerbed: number[], n: number): boolean {
   if (n === 0) {
     return true;
@@ -26,7 +30,7 @@ export function canPlaceFlowersCopied(flowerbed: number[], n: number): boolean {
     const current = flowerbed[i];
     const prev = flowerbed[i - 1];
     const next = flowerbed[i + 1];
-    // !0 = not falsy ~ truthy -> this is empty
+    // !0 = not falsy ~ truthy -> this is empty.
     if (!current && !prev && !next) {
       count++;
       flowerbed[i] = 1;
@@ -34,4 +38,20 @@ export function canPlaceFlowersCopied(flowerbed: number[], n: number): boolean {
   }
 
   return count >= n;
+}
+
+/**
+ * @description: https://leetcode.com/problems/assign-cookies/
+ * tldr: compare two array, g and s. Return number of times item of s >= item of g
+ * both g and s are sorted
+ * */
+export function assignCookies(g: number[], s: number[]) {
+  // if you have 2 item in s, then you can at most make 2 kids happy
+  let happy = 0;
+  for (let i = 0; i < s.length && happy < g.length; i++) {
+    if (s[i] >= g[i]) {
+      happy++;
+    }
+  }
+  return happy;
 }
