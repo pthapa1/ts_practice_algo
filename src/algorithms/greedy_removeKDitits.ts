36ae9d37713bdae5de6f93f816d77dd1a6388d2c
Pratik Thapa <pratik.philos@live.com>
2024-10-05 12:07:38 -0400

    remove k digits
    I learned about overflowing and how to avoid it in loop.
    I leared about how to not overthink stuff.
    Learn standard Library

---
+++ src/algorithms/greedy_removeKDitits.ts
@@ -0,0 +1,33 @@
+/**
+ * @description  Remove K digits
+Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
+
+Example 1:
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+
+Example 2:
+Input: num = "10200", k = 1
+Output: "200"
+Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+
+Example 3:
+Input: num = "10", k = 2
+Output: "0"
+Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+ * */
+function removeKDigits(nums: string, k: number) {
+  let maxNumber = 0;
+  for (let i = 0; i <= nums.length - k; i++) {
+    let extractedString = "";
+    let extractedNumber = 0;
+    for (let j = 0; j < k; j++) {
+      extractedString += nums[i + j]; // access next digit
+      extractedNumber = Number(extractedString);
+    }
+    maxNumber = Math.max(maxNumber, extractedNumber);
+  }
+  const valToReturn = nums.replace(String(maxNumber), "");
+  return valToReturn;
+}
